1db7917d78720fa6d9159c9f7aba7187da639335
Hamza <hamza123ali45678@gmail.com>
2025-12-17 19:33:19 +0500

    adjust rootLayout for home movement

--- src/Root/Pages/RootLayout.tsx
+++ src/Root/Pages/RootLayout.tsx
@@ -1,15 +1,21 @@
 import TopBar from "@/components/ui/shared/TopBar";
-import SideBar from "@/components/ui/shared/sidebar/SideBar";
 import BottomBar from "@/components/ui/shared/BottomBar";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import Sidebar from "../../components/ui/shared/sidebar/SideBar"
+
 function RootLayout() {
+  const [isCollapsed, setisCollapsed] = useState(false)
   return (
     <div className="flex flex-col    min-h-screen  ">
       <TopBar />
       {/* //sidebar + main home page flex  */}
       <div className="flex flex-1 ml-3 mt-2 ">
-        <aside className="hidden md:block md:w-64 lg:w-72  border-gray-800   h-screen">
-      <SideBar />
+        <aside className={`hidden md:block ${isCollapsed ? "w-0" : "w-50 lg:w-72"}  border-gray-800   h-screen`}>
+      <Sidebar
+      iscollapsed={isCollapsed}
+      setiscollapsed={setisCollapsed}
+       />
         </aside>
       <section className=" flex flex-1 p-4 h-full">
         <Outlet />
